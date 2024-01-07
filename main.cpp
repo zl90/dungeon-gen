94b138c32f8ade0911a6772746a60e0b1d6b695c
zl90 <mb541@protonmail.com>
2024-01-07 18:28:14 +1100

    Add random floor tiles + random river generation

--- main.cpp
+++ main.cpp
@@ -2,57 +2,138 @@
 #include <stdio.h>
 #include <array>
 #include <string>
+#include <random>
+#include <time.h>
 
-const uint8_t DEFAULT_GRID_WIDTH = 32;
-const uint8_t DEFAULT_GRID_HEIGHT = 16;
+const uint8_t DEFAULT_GRID_WIDTH = 64;
+const uint8_t DEFAULT_GRID_HEIGHT = 32;
 
-enum class Grid_Item {
-	wood_floor,
-	wood_wall_horizontal,
-	wood_wall_vertical
+enum class Grid_Item
+{
+  floor,
+  wall_horizontal,
+  wall_vertical,
+  floor_2,
+  floor_3,
+  floor_4,
+  water
 };
 
-const char * grid_item_symbol = "-=|";
+const char *grid_symbols = "-=|*\" ~";
 
-class Grid {
+class Grid
+{
 public:
-	Grid(){
-		for (int y = 0; y < DEFAULT_GRID_HEIGHT; y++) {
-			for (int x = 0; x < DEFAULT_GRID_WIDTH; x++) {
-				items[x][y] = Grid_Item::wood_floor;
-			}	
-		}
-	}
+  Grid()
+  {
+    for (int x = 0; x < DEFAULT_GRID_WIDTH; x++)
+    {
+      for (int y = 0; y < DEFAULT_GRID_HEIGHT; y++)
+      {
+        // if (x == 0 || x == DEFAULT_GRID_WIDTH - 1)
+        // {
+        //   items[x][y] = Grid_Item::wall_vertical;
+        // }
+        // else if (y == 0 || y == DEFAULT_GRID_HEIGHT - 1)
+        // {
+        //   items[x][y] = Grid_Item::wall_horizontal;
+        // }
+        // else
+        {
+          items[x][y] = get_random_floor_item();
+        }
+      }
+    }
 
-	void draw() {
-		int row, col;
-		getmaxyx(stdscr, row, col);
-		
-		for (int y = 0; y < DEFAULT_GRID_HEIGHT; y++) {
-			std::string line = "";
-			
-			for (int x = 0; x < DEFAULT_GRID_WIDTH; x++) {
-				line += grid_item_symbol[(int)items[x][y]];
-			}	
+    generate_river();
+  }
 
-			line += '\n';
-			mvprintw(row/2 - y + DEFAULT_GRID_HEIGHT / 2, (col - line.length())/2, "%s", line.c_str());
-		}
-	}
+  void generate_river()
+  {
+    for (int y = 0; y < DEFAULT_GRID_HEIGHT; y++)
+    {
+      if (y == 0)
+      {
+        const short random_int = rand() % DEFAULT_GRID_WIDTH;
+        items[random_int][y] = Grid_Item::water;
+      }
+      else
+      {
+        for (int x = 0; x < DEFAULT_GRID_WIDTH; x++)
+        {
+          if (items[x][y - 1] == Grid_Item::water || items[x - 1][y - 1] == Grid_Item::water || items[x + 1][y - 1] == Grid_Item::water)
+          {
+            if (rand() % 2 == 1)
+            {
+              items[x][y] = Grid_Item::water;
+            }
+          }
+        }
+      }
+    }
+  }
+
+  void draw()
+  {
+    int row, col;
+    getmaxyx(stdscr, row, col);
+
+    for (int y = 0; y < DEFAULT_GRID_HEIGHT; y++)
+    {
+      for (int x = 0; x < DEFAULT_GRID_WIDTH; x++)
+      {
+        if (items[x][y] == Grid_Item::water)
+        {
+          attron(COLOR_PAIR(1));
+        }
+        mvaddch(row / 2 - y + DEFAULT_GRID_HEIGHT / 2, col / 2 - DEFAULT_GRID_WIDTH / 2 + x, grid_symbols[(int)items[x][y]]);
+        if (items[x][y] == Grid_Item::water)
+        {
+          attroff(COLOR_PAIR(1));
+        }
+      }
+
+      addch('\n');
+    }
+  }
+
+  Grid_Item get_random_floor_item()
+  {
+    const short random_int = std::rand() % 4;
+    switch (random_int)
+    {
+    case 0:
+      return Grid_Item::floor;
+    case 1:
+      return Grid_Item::floor_2;
+    case 2:
+      return Grid_Item::floor_3;
+    case 3:
+      return Grid_Item::floor_4;
+    default:
+      return Grid_Item::floor;
+    }
+  }
 
 private:
-	std::array<std::array<Grid_Item, DEFAULT_GRID_HEIGHT>, DEFAULT_GRID_WIDTH> items;
+  std::array<std::array<Grid_Item, DEFAULT_GRID_HEIGHT>, DEFAULT_GRID_WIDTH> items;
 };
 
 int main()
 {
-	initscr();
+  std::srand(time(nullptr));
 
-	Grid g;
-	g.draw();
+  initscr();
 
-	refresh();
-	getch();
-	endwin();
-	return 0;
+  start_color();
+  init_pair(1, COLOR_BLUE, COLOR_BLACK);
+
+  Grid g;
+  g.draw();
+
+  refresh();
+
+  getch();
+  endwin();
+  return 0;
 }
