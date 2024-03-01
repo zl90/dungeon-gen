91ba833b179ab60f75b4b55ee2182542f497f0ce
zl90 <mb541@protonmail.com>
2024-03-01 22:04:48 +1100

    Add hotspots

--- heatmap.cpp
+++ heatmap.cpp
@@ -3,11 +3,15 @@
 
 HeatMap::HeatMap(unsigned int x, unsigned int y) {
   double max_number_of_hotspots = rand() % x / 10;
-  double hotspot_probability = max_number_of_hotspots / (x * y);
+  double hotspot_probability = (x * y) / max_number_of_hotspots;
 
   for (unsigned int i = 0; i < x; i++) {
     for (unsigned int j = 0; j < y; j++) {
       heatmap[i][j] = 0.0;
+
+      if (rand() % (x * y) < hotspot_probability) {
+        heatmap[i][j] = 1.0;
+      }
     }
   }
 }
