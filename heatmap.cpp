c1d3f88212f4cc7263553e6a6e5d5f209a6ec0f9
zl90 <mb541@protonmail.com>
2024-03-02 23:10:49 +1100

    Prepare convolution calculation

--- heatmap.cpp
+++ heatmap.cpp
@@ -16,4 +16,31 @@ HeatMap::HeatMap(unsigned int x, unsigned int y) {
       }
     }
   }
+
+  for (int k = 0; k < 10; k++) {
+    // Perform convolution 10 times to smooth the heatmap
+    for (unsigned int i = 0; i < x; i++) {
+      for (unsigned int j = 0; j < y; j++) {
+        if (i == 0 && j == 0) {
+          // top left corner
+        } else if (i == x - 1 && j == 0) {
+          // top right corner
+        } else if (i == 0 && j == y - 1) {
+          // bottom left corner
+        } else if (i == x - 1 && j == y - 1) {
+          // bottom right corner
+        } else if (i == 0) {
+          // leftmost column
+        } else if (i == x - 1) {
+          // rightmost column
+        } else if (j == 0) {
+          // top row
+        } else if (j == y - 1) {
+          // bottom row
+        } else [[likely]] {
+          // normal convolution
+        }
+      }
+    }
+  }
 }
