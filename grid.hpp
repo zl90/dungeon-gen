05c1ce7c93ebfa3aabb39e24a7c208396afdb427
zl90 <mb541@protonmail.com>
2024-03-05 22:39:30 +1100

    Remove unused function declarations

--- grid.hpp
+++ grid.hpp
@@ -41,16 +41,6 @@ private:
 
   Grid_Item get_random_water_tile();
 
-  bool is_water_tile(int x, int y);
-
-  bool is_above_river_tile(int x, int y);
-
-  void generate_ground_tiles();
-
-  void generate_river_tiles();
-
-  void generate_mountain_tiles();
-
   void set_colour_for_item(Grid_Item item);
 
   void unset_colour();
